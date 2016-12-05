a6f531fa3940aea54c6a63a402047f4a31a81900
maparker <maparker@bauer-media.com.au>
2016-12-05 11:40:24 +1100

    React Test Add To Cart

---
+++ src/js/components/app.js
@@ -0,0 +1,19 @@
+/** @jsx React.DOM */
+var React = require('react');
+var Catalog = require('../components/app-catalog');
+var Cart = require('../components/app-cart');
+var App = React.createClass({ 
+	render: function(){
+		return (
+			<div>
+			<h1>Lets Shop</h1>
+			<Catalog />
+			<h1>Cart</h1>
+			<Cart />
+			</div>
+		)
+	}
+});
+
+module.exports = App;
+	
